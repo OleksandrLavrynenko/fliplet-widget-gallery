ade65bddec17c092537c32f472c01b3062996ebc
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-09-15 13:42:32 +0300

    [ADD] start translation

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -15,11 +15,11 @@ this["Fliplet"]["Widget"]["Templates"]["templates.image"] = Handlebars.template(
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\" data-file-ext=\""
     + alias4(((helper = (helper = helpers.ext || (depth0 != null ? depth0.ext : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ext","hash":{},"data":data}) : helper)))
-    + "\">\n    <div class=\"image-holder\" style=\"background-image: url('"
+    + "\">\r\n    <div class=\"image-holder\" style=\"background-image: url('"
     + alias4(((helper = (helper = helpers.urlSmall || (depth0 != null ? depth0.urlSmall : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"urlSmall","hash":{},"data":data}) : helper)))
-    + "');\"\n         style=\"{color: blue; background: white}\">\n        <div class=\"image-overlay\">\n            <i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i>\n        </div>\n    </div>\n    <div class=\"image-title\">\n        <p>\n            <span class=\"title-text "
+    + "');\"\r\n         style=\"{color: blue; background: white}\">\r\n        <div class=\"image-overlay\">\r\n            <i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i>\r\n        </div>\r\n    </div>\r\n    <div class=\"image-title\">\r\n        <p>\r\n            <span class=\"title-text "
     + ((stack1 = helpers.unless.call(alias1,(depth0 != null ? depth0.title : depth0),{"name":"unless","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + " \">"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.title : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</span>\n        </p>\n    </div>\n</div>";
+    + "</span>\r\n        </p>\r\n    </div>\r\n</div>";
 },"useData":true});
